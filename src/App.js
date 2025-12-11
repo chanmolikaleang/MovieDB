6acc205c8b62048eb4670421d69051ba1ddb1d6c
Leang Chanmolika <molikaleang9@gmail.com>
2025-12-11 21:16:51 +0700

    Search title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import LandingPage from "./components/LandingPage";
 import MovieDetails from "./components/MovieDetail";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/movie/:id" element={<MovieDetails />} />
